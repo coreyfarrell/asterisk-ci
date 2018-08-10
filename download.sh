6f2a299e1dc92daf1b3ae946b0ba2e3b86b9eac8
Corey Farrell <git@cfware.com>
2018-08-10 13:25:20 -0400

    updates

--- download.sh
+++ download.sh
@@ -4,9 +4,7 @@ source $(dirname $0)/shared.sh
 
 console_title "Downloading results from container."
 # Update ./output and ./testsuite from docker volume.
-runner git rm -qfr output testsuite
+runner git rm -qfr output
 echo "+ sudo docker run --workdir /usr/src/asterisk/tests/CI ${DOCKER_ARGS} tar -cf - output | tar xf -"
 sudo docker run --workdir /usr/src/asterisk/tests/CI ${DOCKER_ARGS} tar -cf - output | tar xf -
-echo "+ sudo docker run --workdir /usr/src ${DOCKER_ARGS} tar -cf - testsuite/asterisk-test-suite-report.xml testsuite/logs/ | tar xf -"
-sudo docker run --workdir /usr/src ${DOCKER_ARGS} tar -cf - testsuite/asterisk-test-suite-report.xml testsuite/logs/ | tar xf -
 runner git add output testsuite
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source $(dirname $0)/shared.sh
 
 console_title "Running asterisk-ci-full."
 # Build, install, unit tests, testsuite and process coverage.
-runner sudo docker run --privileged ${DOCKER_ARGS} asterisk-ci-full
+runner sudo docker run --privileged ${DOCKER_ARGS} asterisk-ci-full "$@"
