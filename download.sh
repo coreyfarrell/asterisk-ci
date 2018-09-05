c824b61e690a69300c1c863206a7d8f948510121
Corey Farrell <git@cfware.com>
2018-09-05 15:03:18 -0400

    Tweak scripts

--- download.sh
+++ download.sh
@@ -3,8 +3,7 @@ set -e
 source $(dirname $0)/shared.sh
 
 console_title "Downloading results from container."
-# Update ./output and ./testsuite from docker volume.
-runner git rm -qfr output
+# Update ./output from docker volume.
+runner rm -fr output || true
 echo "+ sudo docker run --workdir /usr/src/asterisk/tests/CI ${DOCKER_ARGS} tar -cf - output | tar xf -"
 sudo docker run --workdir /usr/src/asterisk/tests/CI ${DOCKER_ARGS} tar -cf - output | tar xf -
-runner git add output testsuite
